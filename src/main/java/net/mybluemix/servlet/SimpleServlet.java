3a4b206a64fd3922550c3e2e6427f62ac5899d7a
Yusuke Suzuki <elphelt.valentine.161@gmail.com>
2016-11-08 16:48:21 +0900

    Change war

--- src/main/java/net/mybluemix/servlet/SimpleServlet.java
+++ src/main/java/net/mybluemix/servlet/SimpleServlet.java
@@ -2,11 +2,13 @@ package net.mybluemix.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 /**
  * 起動クラスです。
  */
 @SpringBootApplication  // same as @Configuration @EnableAutoConfiguration @ComponentScan
-public class SimpleServlet {
+public class SimpleServlet extends SpringBootServletInitializer {
     /**
      * 処理を実行します。
      * @param args 引数
@@ -14,5 +16,10 @@ public class SimpleServlet {
      */
     public static void main(String[] args) throws Exception {
         SpringApplication.run(SimpleServlet.class, args);
+    }
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SimpleServlet.class);
     }
 }
