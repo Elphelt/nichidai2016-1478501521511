9e9727d2f9bbc905551cee801c1b349124620729
Yusuke Suzuki <elphelt.valentine.161@gmail.com>
2016-11-16 09:16:35 +0900

    Mod Server Sockjs

--- src/main/java/net/mybluemix/config/WebSocketConfig.java
+++ src/main/java/net/mybluemix/config/WebSocketConfig.java
@@ -12,7 +12,7 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer { 
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/hello").setAllowedOrigins("*").withSockJS(); // WebSocketのエンドポイント (接続時に指定するエンドポイント)を指定
+        registry.addEndpoint("/hello").setAllowedOrigins("*").withSockJS().setClientLibraryUrl("//cdnjs.cloudflare.com/ajax/libs/sockjs-client/1.1.1/sockjs.js"); // WebSocketのエンドポイント (接続時に指定するエンドポイント)を指定
     }
 
     @Override
