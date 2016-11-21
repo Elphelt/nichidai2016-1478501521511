c698628c3c4096f161b1c038727f4f913ef992c9
Yusuke Suzuki <elphelt.valentine.161@gmail.com>
2016-11-21 16:11:47 +0900

    Mod dengon

--- client/nichidai2016/src/app/dengon/dengon.component.ts
+++ client/nichidai2016/src/app/dengon/dengon.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+var SockJS = require('sockjs-client');
+var Stomp = require('stompjs');
+
 @Component({
   selector: 'app-dengon',
   templateUrl: './dengon.component.html',
@@ -7,9 +10,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DengonComponent implements OnInit {
 
+  private stompClient: any;
+  private showPlayer: boolean = false;
+  private showDisplay: boolean = false;
+
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  ngOnDestroy() {
+    if (this.stompClient != null) {
+        this.stompClient.disconnect();
+    }
   }
+  
+  setConnected(connected) {
+    document.getElementById('connect').style.visibility = !connected ? 'visible' : 'hidden';
+    document.getElementById('disconnect').style.visibility = connected ? 'visible' : 'hidden';
+    document.getElementById('conversationDiv').style.visibility = connected ? 'visible' : 'hidden';
+  }
+
+  connect() {
+    var that = this;
+    var socket = new SockJS('/hello');
+    this.stompClient = Stomp.over(socket);
+    this.stompClient.connect({}, function (frame) {
+      console.log('Connected: ' + frame);
+      that.stompClient.subscribe('/topic/admin', function (greeting) {
+      });
+    }, function (err) {
+      console.log('err', err);
+    });
+    this.setConnected(true);
+  }
+
+
+  disconnect() {
+    if (this.stompClient != null) {
+        this.stompClient.disconnect();
+    }
+    this.setConnected(false);
+    console.log("Disconnected");
+  } 
+
+  private changePlayer(): void {
+    this.showPlayer = true;
+    this.showDisplay = false;
+  }
+
+  private changeDisplay(): void {
+    this.showPlayer = false;
+    this.showDisplay = true;
+  }
+
 
 }
