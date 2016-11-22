152aa9dbc268fb10dfdd60d426788d21af266b3a
Yusuke Suzuki <elphelt.valentine.161@gmail.com>
2016-11-22 15:19:16 +0900

    Add  Image Press

--- src/main/java/net/mybluemix/controller/HelloController.java
+++ src/main/java/net/mybluemix/controller/HelloController.java
@@ -1,8 +1,16 @@
 package net.mybluemix.controller;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Locale;
+
+import javax.imageio.IIOImage;
+import javax.imageio.ImageIO;
+import javax.imageio.ImageWriteParam;
+import javax.imageio.ImageWriter;
+import javax.imageio.plugins.jpeg.JPEGImageWriteParam;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -31,34 +39,45 @@ public class HelloController {
 		return result;
 	}
 
-	@RequestMapping(value="/up", method = RequestMethod.POST )
+	@RequestMapping(value = "/up", method = RequestMethod.POST)
 	public VisualClassification post(@RequestParam MultipartFile multipartFile) throws IOException {
 
 		// ファイルが空の場合は異常終了
 		if (multipartFile.isEmpty()) {
 			// 異常終了時の処理
-			return null; 
-		}else{
+			return null;
+		} else {
 			VisualRecognition service = new VisualRecognition(VisualRecognition.VERSION_DATE_2016_05_20);
 			service.setApiKey("ee08899658034e2f4ca599d2c2ba32da6eaa3435");
+			File imageF = convert(multipartFile);
+			File out = new File("temp.jpg");
+			out.createNewFile();
+			BufferedImage image = ImageIO.read(imageF);
+			JPEGImageWriteParam param = new JPEGImageWriteParam(Locale.getDefault());
+			param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+			param.setCompressionQuality(0.8f);
+
+			ImageWriter writer = ImageIO.getImageWritersByFormatName("jpg").next();
+			writer.setOutput(ImageIO.createImageOutputStream(out));
+			writer.write(null, new IIOImage(image, null, null), param);
+			writer.dispose();
 
 			System.out.println("Classify an image");
-			ClassifyImagesOptions options = new ClassifyImagesOptions.Builder().images(convert(multipartFile)).build();
+			ClassifyImagesOptions options = new ClassifyImagesOptions.Builder().images(out).build();
 			VisualClassification result = service.classify(options).execute();
 
-			System.out.println(result);			
+			System.out.println(result);
 			return result;
 		}
-		
+
 	}
-	
-	public File convert(MultipartFile file) throws IOException
-	{    
-	    File convFile = new File(file.getOriginalFilename());
-	    convFile.createNewFile(); 
-	    FileOutputStream fos = new FileOutputStream(convFile); 
-	    fos.write(file.getBytes());
-	    fos.close(); 
-	    return convFile;
+
+	public File convert(MultipartFile file) throws IOException {
+		File convFile = new File(file.getOriginalFilename());
+		convFile.createNewFile();
+		FileOutputStream fos = new FileOutputStream(convFile);
+		fos.write(file.getBytes());
+		fos.close();
+		return convFile;
 	}
 }
