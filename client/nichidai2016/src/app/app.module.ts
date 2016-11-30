87a85da4e5dc3958fc65bd9d65c8f067a2ab9dca
Yusuke Suzuki <elphelt.valentine.161@gmail.com>
2016-11-30 17:27:43 +0900

    Mod

--- client/nichidai2016/src/app/app.module.ts
+++ client/nichidai2016/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, enableProdMode } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
@@ -13,8 +13,6 @@ import { DengonComponent } from './dengon/dengon.component';
 
 import { FileSelectDirective } from 'ng2-file-upload';
 import { VisualRecoComponent } from './visual-reco/visual-reco.component';
-
-enableProdMode();
 
 @NgModule({
   declarations: [
--- src/main/webapp/main.bundle.js
+++ src/main/webapp/main.bundle.js
@@ -55248,7 +55248,6 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 
 
 
-__webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1__angular_core__["enableProdMode"])();
 var AppModule = (function () {
     function AppModule() {
     }
@@ -55771,12 +55770,8 @@ var VisualRecoComponent = (function () {
 
 "use strict";
 /* harmony export (binding) */ __webpack_require__.d(exports, "a", function() { return environment; });
-// The file contents for the current environment will overwrite these during build.
-// The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `angular-cli.json`.
 var environment = {
-    production: false
+    production: true
 };
 
 
